283643de132d1043706dff9ad8b433e04f7e7ddc
Charles Mugford <charles.mugford@artisgroup.com.au>
2020-03-11 13:38:10 +1030

    Added new workflow steps
    New Workflow Steps to:
     - Generate Sales Order From Opportuntity
     - Win an Opportunity

---
+++ msdyncrmWorkflowTools/msdyncrmWorkflowTools/Class/GenerateSalesOrderFromOpportunity.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+
+namespace msdyncrmWorkflowTools
+{
+    public class GenerateSalesOrderFromOpportunity : CodeActivity
+    {
+        #region "Parameter Definition"
+        [RequiredArgument]
+        [Input("Opportunity")]
+        [ReferenceTarget("opportunity")]
+        public InArgument<EntityReference> Opportunity { get; set; }
+        #endregion
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            #region "Load CRM Service from context"
+
+            Common objCommon = new Common(executionContext);
+            objCommon.tracingService.Trace("Load CRM Service from context --- OK");
+
+            #endregion
+
+            #region "Read Parameters"
+            EntityReference opportunity = this.Opportunity.Get(executionContext);
+            if (opportunity == null)
+            {
+                return;
+            }
+
+            objCommon.tracingService.Trace("OpportunityID=" + opportunity.Id);
+            #endregion
+
+            #region "Generate Sales Order From Opportunity Execution"
+            var generateSalesOrderFromOpportunityRequest = new GenerateSalesOrderFromOpportunityRequest
+            {
+                OpportunityId = opportunity.Id
+            };
+
+            var generateSalesOrderFromOpportunityResponse =
+                (GenerateSalesOrderFromOpportunityResponse)objCommon.service.Execute(generateSalesOrderFromOpportunityRequest);
+            Console.WriteLine("  Executed OK.");
+            #endregion
+        }
+    }
+}
---
+++ msdyncrmWorkflowTools/msdyncrmWorkflowTools/Class/WinOpportunity.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+
+namespace msdyncrmWorkflowTools
+{
+    public class WinOpportunity : CodeActivity
+    {
+        #region "Parameter Definition"
+        [RequiredArgument]
+        [Input("Opportunity")]
+        [ReferenceTarget("opportunity")]
+        public InArgument<EntityReference> Opportunity { get; set; }
+        #endregion
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+
+            #region "Load CRM Service from context"
+
+            Common objCommon = new Common(executionContext);
+            objCommon.tracingService.Trace("Load CRM Service from context --- OK");
+
+            #endregion
+
+            #region "Read Parameters"
+            EntityReference opportunity = this.Opportunity.Get(executionContext);
+            if (opportunity == null)
+            {
+                return;
+            }
+
+            objCommon.tracingService.Trace("OpportunityID=" + opportunity.Id);
+            #endregion
+
+            #region "WinOpportuntity Execution"
+            var winOpportunityReq = new WinOpportunityRequest
+            {
+                Status = new OptionSetValue()
+            };
+
+            winOpportunityReq.OpportunityClose = new Entity
+            {
+                LogicalName = "opportunityclose"
+            };
+
+            winOpportunityReq.OpportunityClose.Attributes.Add("OpportunityId", new EntityReference(opportunity.LogicalName, opportunity.Id));
+
+            var winOpportunityRes =
+                (WinOpportunityResponse)objCommon.service.Execute(winOpportunityReq);
+            Console.WriteLine("  Executed OK.");
+            #endregion
+        }
+    }
+}
